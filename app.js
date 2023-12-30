04facead308ed4e01436889d504575d574ac4db8
vandit-kalathiya <vanditkalathiya18@gmail.com>
2023-12-30 22:00:54 +0530

    deleted

--- app.js
+++ app.js
@@ -21,11 +21,7 @@ main()
     .catch(err => console.log(err))
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        family: 4,
-    });
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
